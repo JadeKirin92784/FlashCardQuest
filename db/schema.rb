a8eeceb14142a7054835343e5345df452f895775
mason kamolpechara <mason.kamolpechara@gmail.com>
2024-12-20 23:56:55 +0900

    created user decks

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_11_25_113004) do
+ActiveRecord::Schema[7.1].define(version: 2024_12_20_145557) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -60,6 +60,11 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_25_113004) do
   create_table "flashcards", force: :cascade do |t|
     t.text "question"
     t.text "answer"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "user_decks", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
