e050e8c1d7d6348e6a5e57075512441f36ddcd61
mason kamolpechara <mason.kamolpechara@gmail.com>
2025-01-02 23:52:10 +0900

    added references

--- db/migrate/20241220145557_create_user_decks.rb
+++ db/migrate/20241220145557_create_user_decks.rb
@@ -1,6 +1,8 @@
 class CreateUserDecks < ActiveRecord::Migration[7.1]
   def change
     create_table :user_decks do |t|
+      t.references :user, null: false, foreign_key: true
+      t.references :deck, null: false, foreign_key: true
 
       t.timestamps
     end
