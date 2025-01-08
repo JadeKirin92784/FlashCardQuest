c38b2819c47c41e5530c5be75b67be5856902219
mason kamolpechara <mason.kamolpechara@gmail.com>
2025-01-09 04:15:30 +0900

    doesn't work

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,9 +1,20 @@
 import { Application } from "@hotwired/stimulus"
 
+// Start the Stimulus application
 const application = Application.start()
 
-// Configure Stimulus development experience
+// Import all controllers manually
+import FlashcardController from "./controllers/flashcard_controller.js"
+import TimerController from "./controllers/timer_controller.js"
+// Add more controllers as needed
+
+// Load controllers into Stimulus
+application.register("flashcard", FlashcardController)
+application.register("timer", TimerController)
+// Register additional controllers as needed
+
+// Configure Stimulus development experience (optional)
 application.debug = false
-window.Stimulus   = application
+window.Stimulus = application
 
 export { application }
