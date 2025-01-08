b8f9d8dcb8b4a75769569ee1068bd5463c682d6d
mason kamolpechara <mason.kamolpechara@gmail.com>
2025-01-09 04:36:34 +0900

    can flip cards now

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,20 +1,9 @@
 import { Application } from "@hotwired/stimulus"
 
-// Start the Stimulus application
 const application = Application.start()
 
-// Import all controllers manually
-import FlashcardController from "./controllers/flashcard_controller.js"
-import TimerController from "./controllers/timer_controller.js"
-// Add more controllers as needed
-
-// Load controllers into Stimulus
-application.register("flashcard", FlashcardController)
-application.register("timer", TimerController)
-// Register additional controllers as needed
-
-// Configure Stimulus development experience (optional)
+// Configure Stimulus development experience
 application.debug = false
-window.Stimulus = application
+window.Stimulus   = application
 
 export { application }
